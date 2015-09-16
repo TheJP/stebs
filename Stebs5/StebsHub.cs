8e95d3a41f5ac3e06c5ac291cde308b876641d85
thejp <jp_55555@hotmail.com>
2015-09-16 14:37:49 +0200

    Initialized project

---
+++ Stebs5/StebsHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Stebs5
+{
+    public class StebsHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
