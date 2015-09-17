ade4afffb4752eb0038fbdfd46fc6a2b119ef690
thejp <jp_55555@hotmail.com>
2015-09-17 10:42:10 +0200

    Reworked canvas setup

--- Stebs5/Scripts/main.js
+++ Stebs5/Scripts/main.js
@@ -1,6 +1,18 @@
 ﻿var stebs = {};
 
 /**
+ * Stores a global reference of the canvas and sets the global style.
+ */
+stebs.setupCanvas = function setupCanvas() {
+    stebs.ctx = $('#canvas')[0].getContext('2d');
+    stebs.canvas = stebs.ctx.canvas;
+    stebs.normalizeCanvas();
+
+    stebs.ctx.font = '20pt Helvetica';
+    stebs.ctx.textAlign = 'center';
+}
+
+/**
  * Resize canvas to real size (otherwise the content gets stretched).
  */
 stebs.normalizeCanvas = function normalizeCanvas(ctx) {
@@ -15,20 +27,15 @@ stebs.normalizeCanvas = function normalizeCanvas(ctx) {
 /**
  * Hello callback function.
  */
-stebs.hello = function hello(word){
+stebs.hello = function hello(word) {
     stebs.ctx.fillStyle = 'green';
     stebs.ctx.font = '100pt Helvetica';
-    stebs.ctx.textAlign = 'center';
     stebs.ctx.fillText(word, stebs.canvas.width / 2, stebs.canvas.height / 2);
 };
 
 $(document).ready(function (){
 
-    stebs.ctx = $('#canvas')[0].getContext('2d');
-    stebs.canvas = stebs.ctx.canvas;
-    stebs.normalizeCanvas();
     var hub = $.connection.stebsHub;
-
     hub.client.hello = stebs.hello;
 
     $.connection.hub.start().done(function () {
