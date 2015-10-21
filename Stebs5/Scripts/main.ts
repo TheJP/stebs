c0b530b361cc5a0e7aadad02c010e8ede7e76d36
the-s-a-m <s.merki1991@gmail.com>
2015-10-21 11:50:48 +0200

    added button ram and output flip

--- Stebs5/Scripts/main.ts
+++ Stebs5/Scripts/main.ts
@@ -1,7 +1,9 @@
 ﻿module Stebs {
     export var visible = {
         devices: false,
-        architecture: false
+        architecture: false,
+        ram: false,
+        output: false
     };
 
     var ctx: CanvasRenderingContext2D;
@@ -62,6 +64,36 @@
             var animation2 = { left: animation.left, width: (visible.architecture ? '+=400px' : '-=400px') };
             $('#codingView').animate(animation2);
             visible.architecture = !visible.architecture;
+        },
+
+        /**
+         * Opens/Closes the ram bar.
+         */
+        toggleRAM(): void {
+            if (visible.output) {
+                $('#editorWindow').css({ height: visible.ram ? 'calc(100vh - 38px - 38px - 38px - 100px - 150px)' : 'calc(100vh - 38px - 38px - 38px - 100px - 300px)' });
+            } else {
+                $('#editorWindow').css({ height: visible.ram ? 'calc(100vh - 38px - 38px - 38px - 100px)' : 'calc(100vh - 38px - 38px - 38px - 100px - 150px)' });
+            }
+            $('.ram-container').hide(visible.ram);
+            $('.ram-container').show(!visible.ram);
+            $('.ram').css({ height: visible.ram ? '38px' : 'calc(38px + 150px)' });
+            visible.ram = !visible.ram;
+        },
+
+        /**
+         * Opens/Closes the output bar.
+         */
+        toggleOutput(): void {
+            if (visible.ram) {
+                $('#editorWindow').css({ height: visible.output ? 'calc(100vh - 38px - 38px - 38px - 100px - 150px)' : 'calc(100vh - 38px - 38px - 38px - 100px - 300px)' });
+            } else {
+                $('#editorWindow').css({ height: visible.output ? 'calc(100vh - 38px - 38px - 38px - 100px)' : 'calc(100vh - 38px - 38px - 38px - 100px - 150px)' });
+            }
+            $('.output-container').hide(visible.output);
+            $('.output-container').show(!visible.output);
+            $('.output').css({ height: visible.output ? '38px' : 'calc(38px + 150px)'});
+            visible.output = !visible.output;
         }
     };
 }
@@ -87,6 +119,8 @@ $(document).ready(function (){
 
     $('#openDevices').click(Stebs.ui.toggleDevices);
     $('#openArchitecture').click(Stebs.ui.toggleArchitecture);
+    $('#openRam').click(Stebs.ui.toggleRAM);
+    $('#openOutput').click(Stebs.ui.toggleOutput);
     $(window).resize(function windowResize() {
         var minus = 50 + (Stebs.visible.architecture ? 400 : 0) + (Stebs.visible.devices ? 400 : 0);
         $('#codingView').css('width', (window.innerWidth -  minus) + 'px');
