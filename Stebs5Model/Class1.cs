bacd1b1d31e8af98cd3c8dfd9a8d879d6a232ba3
TheJP <jp_55555@hotmail.com>
2015-12-16 13:43:45 +0100

    Updated owin and installed identity and ef6

---
+++ Stebs5Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stebs5Model
+{
+    public class Class1
+    {
+    }
+}
