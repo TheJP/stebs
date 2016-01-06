19c2956a656ba3f42a7b85c7c27129c5b057680e
TheJP <jp_55555@hotmail.com>
2016-01-06 14:05:18 +0100

    Update of packages

--- Stebs5Model/StebsUser.cs
+++ Stebs5Model/StebsUser.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Stebs5Model
 {
     public class StebsUser : IdentityUser
     {
         public virtual FileSystem FileSystem { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<StebsUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
     }
 }
